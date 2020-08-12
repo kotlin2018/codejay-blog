f3e2489e20cb1d0d2a9e1a7c8f6afb72fd110aea
Jay-wj <65354888+Jay-wj@users.noreply.github.com>
2020-08-12 16:28:32 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 axios.defaults.withCredentials=true
 // 1.创建axios实例
 const instance = axios.create({
-  baseURL: '/api', // 后台地址
+  baseURL: 'http://XXX.XXX.X.XXX:XXXX', // 后台地址
   timeout: 5000,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded' 
