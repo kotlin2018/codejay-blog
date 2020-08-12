263155662fc41f8f1f48d46dd79337520c6b9ca9
Jay-wj <65354888+Jay-wj@users.noreply.github.com>
2020-08-12 16:29:29 +0800

    Update uploadImage.js

--- src/network/uploadImage.js
+++ src/network/uploadImage.js
@@ -1,6 +1,6 @@
 export default {
   UPLOADIMG: {
-    BASEURL: 'http://192.168.1.106:3000', // 后台地址
+    BASEURL: 'http://XXX.XXX.X.XXX:XXXX', // 后台地址
     UPLOADURL: '/upload', // 文章封面图路径
     UPLOADIMAGEURL: '/articleImages', // 文章内容插图
     UPLOADAVATAR: '/userAvatar', // 用户头像
